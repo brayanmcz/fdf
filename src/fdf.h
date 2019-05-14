cc3d6caff0efff41f7613f4c0363bba2db693fa2
Brayan Castro <brayan@Brayans-MacBook-Pro.local>
2019-05-14 15:45:27 -0700

    Completed a grid

--- src/fdf.h
+++ src/fdf.h
@@ -3,25 +3,25 @@
 /*                                                        :::      ::::::::   */
 /*   fdf.h                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bcastro <bcastro@student.42.fr>            +#+  +:+       +#+        */
+/*   By: bcastro <bcastro@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/06 19:11:06 by brayan            #+#    #+#             */
-/*   Updated: 2019/05/08 18:09:00 by bcastro          ###   ########.fr       */
+/*   Updated: 2019/05/08 18:23:44 by brayan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FDF_H
 # define FDF_H
-# include "../lib/minilibx/mlx.h"
-# include "../lib/libft/libft.h"
+# include "../minilibx/mlx.h"
+# include "../libft/libft.h"
 # include "stdlib.h"
 # include "stdio.h" //DELETE THIS before project submitted
+# include "unistd.h"
 
 typedef	struct	s_point {
-	unsigned char x;         //x position on map
- 	unsigned char y;         //y position on map
-	unsigned char z;         //z poistion on map
-	struct s_point *nbrs[2]; //2 possible neighbors (down and right)
+	int x;   //x position on map
+ 	int y;   //y position on map
+	int z;   //z poistion on map
 } t_point;
 
 // Bresenham Line Algorithm
